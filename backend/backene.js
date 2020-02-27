acd20123234469e66e4b8ed6d3086caf7d0f2d44
unknown <antintue@gmail.com>
2020-02-27 21:15:53 +0800

    can download videos

--- backend/backene.js
+++ backend/backene.js
@@ -1,15 +1,28 @@
 var fetch = require('node-fetch')
-
+var fs = require('fs')
 
 var api_url = ' https://d17lx9ucc6k9fc.cloudfront.net/studioclassroom/202002/SC20200228.mp4'
-fetch(api_url, {})
+
+
+var   downloadBlock = async function(url){
+  
+  return await fetch(api_url, {})
   .then((response) => {
     // 這裡會得到一個 ReadableStream 的物件
     console.log(response);
     // 可以透過 blob(), json(), text() 轉成可用的資訊
-    return response.json();
+    return response.buffer();
   }).then((jsonData) => {
-    console.log(jsonData);
-  }).catch((err) => {
-    console.log('錯誤:', err);
-  });
+    fs.writeFileSync('./source/test.mp4',jsonData)
+    //console.log('jsonData = ' +jsonData);
+  }).bind(this)
+
+}
+
+(async function () {  
+
+ var buffer = downloadBlock(api_url)
+  console.log(buffer)
+  fs.writeFileSync('./source/test.mp4',buffer)
+
+})
